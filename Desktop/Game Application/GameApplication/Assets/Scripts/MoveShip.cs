274a52548957f23d75441c84c5e29de98aa7936f
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2020-09-28 13:56:33 +0300

    First_Commit

---
+++ Desktop/Game Application/GameApplication/Assets/Scripts/MoveShip.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShip : MonoBehaviour
+{
+    CharacterController cc;
+    Vector3 moveVec;
+    public Animator Ship;
+    public GameManager GM;
+    public bool CanPlay;
+
+
+    int laneNumber = 1,
+        lanesCount = 2;
+
+    public float speed = 5;
+
+    public float FirstLanePos,
+                LaneDistance,
+                SideSpeed;
+
+
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        moveVec = new Vector3(1, 0, 0);
+        Ship = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if (CanPlay)
+        {
+            moveVec.x = speed;
+        }
+        
+        moveVec *= Time.deltaTime;
+
+        CheckInput();
+
+
+
+
+        cc.Move(moveVec);
+        Vector3 newPos = transform.position;
+        newPos.z = Mathf.Lerp(newPos.z, FirstLanePos + (laneNumber + LaneDistance), Time.deltaTime * SideSpeed);
+        transform.position = newPos;
+
+    }
+
+    void CheckInput()
+    {
+        if (!CanPlay)
+        {
+            return;
+        }
+        int sign = 0;
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            Ship.Play("Right");
+            sign = 1;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Ship.Play("LeftTurn");
+            sign = -1;
+        }
+        else
+        {
+            return;
+        }
+
+
+
+        laneNumber += sign;
+        laneNumber = Mathf.Clamp(laneNumber, 0, lanesCount);
+
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        StartCoroutine(Death());
+    }
+
+    IEnumerator Death()
+    {
+        CanPlay = false;
+
+        yield return new WaitForSeconds(1);
+
+        GM.ShowResult();
+    }
+
+
+}
+
---
+++ Desktop/Game Application/GameApplication/Assets/Scripts/RoadSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+    public GameObject[] RoadBloksPrefab;
+    public GameObject StartBlock;
+
+    float blockXpos = 0;          
+    int blocksCount = 7;
+    float blocksLenght = 0;
+    int saveZone = 50;
+
+
+
+    public Transform PlayerTransf;
+    List<GameObject> CurrentBlocks = new List<GameObject>();
+
+    Vector3 startPlayerPos;
+
+    void Start()
+    {
+        blockXpos = StartBlock.transform.position.x;
+        blocksLenght = StartBlock.GetComponent<BoxCollider>().bounds.size.x;
+
+        CurrentBlocks.Add(StartBlock);
+
+        for (int i = 0; i < blocksCount; i++)
+            SpawnBlock();
+    }  
+
+
+
+    void Update()
+    {
+        CheckForSpaun();
+    }
+
+
+    void CheckForSpaun()
+    {
+        if (PlayerTransf.position.x - saveZone > (blockXpos - blocksCount * blocksLenght))
+        {
+            SpawnBlock();
+            DestroyBlock();
+        }
+
+    }
+
+
+    void SpawnBlock()
+    {
+
+        GameObject block = Instantiate(RoadBloksPrefab[Random.Range(0, RoadBloksPrefab.Length)], transform);
+
+
+        blockXpos += blocksLenght;
+
+        block.transform.position = new Vector3(blockXpos, 0, 0);
+
+        CurrentBlocks.Add(block);
+    }
+
+    void DestroyBlock()
+    {
+        Destroy(CurrentBlocks[0]);
+        CurrentBlocks.RemoveAt(0);
+    }
+}
